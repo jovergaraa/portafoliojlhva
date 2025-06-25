8e8d44561780d377acaff7e709859a244ee616de
Jose Vergara <joseluisvergaraallende@gmail.com>
2025-06-25 19:50:01 -0400

    portafolio casi listo :)

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -16,44 +16,118 @@ import {
   SiNextdotjs,
   SiSolidity,
   SiPostgresql,
+  SiVisualstudiocode,
+  SiFigma,
+  SiAdobexd,
+  SiHtml5,
+  SiCss3,
+  SiDjango,
+  SiFlask,
+  SiExpress,
+  SiBootstrap,
+  SiIonic,
 } from "react-icons/si";
 import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-     
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
-      </Col>
-    </Row>
+    <div style={{ paddingTop: "20px" }}>
+      {/* Lenguajes de Programación */}
+      <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+        <Col md={12}>
+          <h3 style={{ textAlign: "center", color: "white", marginBottom: "40px" }}>
+            <span className="purple">Lenguajes de Programación</span>
+          </h3>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <DiJavascript1 title="JavaScript" />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <DiPython title="Python" />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <DiJava title="Java" />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <SiHtml5 title="HTML5" />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <SiCss3 title="CSS3" />
+        </Col>
+      </Row>
+
+      {/* Frameworks y Librerías */}
+      <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+        <Col md={12}>
+          <h3 style={{ textAlign: "center", color: "white", marginBottom: "40px" }}>
+            <span className="purple">Frameworks y Librerías</span>
+          </h3>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <DiReact title="React" />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <DiNodejs title="Node.js" />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <SiExpress title="Express.js" />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <SiBootstrap title="Bootstrap" />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <SiIonic title="Ionic" />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <SiDjango title="Django" />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <SiFlask title="Flask" />
+        </Col>
+      </Row>
+
+      {/* Bases de Datos */}
+      <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+        <Col md={12}>
+          <h3 style={{ textAlign: "center", color: "white", marginBottom: "40px" }}>
+            <span className="purple">Bases de Datos</span>
+          </h3>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <SiPostgresql title="PostgreSQL" />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <SiFirebase title="Firebase" />
+        </Col>
+      </Row>
+
+      {/* Herramientas de Desarrollo */}
+      <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+        <Col md={12}>
+          <h3 style={{ textAlign: "center", color: "white", marginBottom: "40px" }}>
+            <span className="purple">Herramientas de Desarrollo</span>
+          </h3>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <DiGit title="Git" />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <SiVisualstudiocode title="VS Code" />
+        </Col>
+      </Row>
+
+      {/* Herramientas de Diseño */}
+      <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+        <Col md={12}>
+          <h3 style={{ textAlign: "center", color: "white", marginBottom: "40px" }}>
+            <span className="purple">Herramientas de Diseño</span>
+          </h3>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons tooltip-container">
+          <SiFigma title="Figma" />
+        </Col>
+      </Row>
+    </div>
   );
 }
 
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,20 +15,17 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          Proyectos en los que he trabajado
         </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
+  
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="IGN TATTO"
+              description="Un proyecto que busca desarrollar una solución informática personalizada para el mundo del tatuaje, con el objetivo de mejorar la gestión de citas, clientes, diseños, inventario y facturación. Además de brindar una experiencia a amantes de los tatuajes generando vínculos e interacciones gracias a una app móvil y una plataforma web "
+              ghLink="https://github.com/distrad/Capstone"
             />
           </Col>
 
@@ -36,8 +33,8 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
+              title="Muebles Gerardo"
+              description="El proyecto consiste en desarrollar un sitio web para una empresa ficticia de muebles establecida desde hace varios años. La solución propuesta es la creación de un sitio web que permita a la empresa mostrar su catálogo de muebles, ofrecer la posibilidad de compras en línea, gestionar pedidos personalizados y ofrecer servicios de reparación. "
               ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
               demoLink="https://blogs.soumya-jit.tech/"
             />
@@ -47,44 +44,10 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              title="Music Pro"
+              description="Proyecto que se centra en el desarrollo de una plataforma informática de gestión de productos para Una empresa de Instrumentos musicales, que permita a sus clientes acceder a una amplia selección de instrumentos y accesorios musicales, a fin de realizar compras de manera fácil y eficiente. "
               ghLink="https://github.com/soumyajit4419/Editor.io"
               demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
         </Row>
