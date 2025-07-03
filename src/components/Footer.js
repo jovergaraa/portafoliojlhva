1b3d7427b2ff96b2ffde1fce7025569a29908d49
Jose Vergara <joseluisvergaraallende@gmail.com>
2025-07-02 23:17:08 -0400

    modificacion footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,7 +35,6 @@ function Footer() {
       <Container fluid className="footer">
         <Row>
           <Col md="4" className="footer-copywright">
-            <h3>Designed and Developed by Jose Vergara</h3>
           </Col>
           <Col md="4" className="footer-copywright">
           </Col>
